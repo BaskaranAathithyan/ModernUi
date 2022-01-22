e7512cf6ba56128ad49ea7eac00d3f47c60afcc4
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-01-22 22:33:31 +0530

    create_folders

---
+++ mine/src/containers/posibility/Posibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './posibility.css';
+
+const Posibility = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Posibility
